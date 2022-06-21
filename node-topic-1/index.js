4aa211ed4970fd687238f390a98fcc7cecbcfdf6
GuliyevMatin <metinquliyev1323@gmail.com>
2022-06-21 23:36:42 +0400

    node js 48

--- node-topic-1/index.js
+++ node-topic-1/index.js
@@ -1,4 +1,4 @@
-// import inquirer from "inquirer";
+import inquirer from "inquirer";
 
 // let pass = "123456"
 // inquirer
@@ -41,10 +41,40 @@
 //       message : "What is your password?",
 //       mask : "*",
 
-
 //     }
 //   ])
 //   .then((answers) => {
 //     answers.password === pass ? console.log(answers) : console.log("Wrong");
 //   });
 
+function Game(name, position, offense, defense) {
+  this.name = name;
+  this.position = position;
+  this.offense = offense;
+  this.defense = defense;
+  goodGame = () => {
+    console.log("dfdf");
+  };
+  badGame = () => {};
+  printstats = () => {};
+}
+
+
+
+let player1 = new Game();
+let player2 = new Game();
+
+// inquirer.prompt([
+//     {
+//         type:"number",
+//         name: "number",
+//         message:"Choose number between [1-5]"
+//     }
+// ]).then((answers)=>{
+//     startGame(answers)
+// })
+
+// function startGame(data){
+//     let randomNum = Math.floor(Math.random() * 5)
+
+// }
