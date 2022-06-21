73d86e362404a8ccd8f74c4c4f596b22e2cb5afb
Metin1323 <95915073+Metin-Quliyev@users.noreply.github.com>
2022-06-21 17:21:43 +0400

    created

---
+++ node-topic-1/index.js
@@ -0,0 +1,50 @@
+// import inquirer from "inquirer";
+
+// let pass = "123456"
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "What is your name?",
+//     },
+//     {
+//       type: "input",
+//       name: "age",
+//       message: "How old are you?",
+//     },
+//     {
+//       type: "checkbox",
+//       name : "city",
+//       message : "Where are you from?",
+//       choices : [
+//         "New York",
+//         "Los Angeles",
+//         "Chicago",
+//       ],
+
+//     }
+//     ,
+//     {
+//       type: "list",
+//       name: "color",
+//       message: "What is your favorite color?",
+//       choices: [
+//         "red",
+//         "blue",
+//       ]
+//     },
+
+//     {
+//       type:"password",
+//       name : "password",
+//       message : "What is your password?",
+//       mask : "*",
+
+
+//     }
+//   ])
+//   .then((answers) => {
+//     answers.password === pass ? console.log(answers) : console.log("Wrong");
+//   });
+
